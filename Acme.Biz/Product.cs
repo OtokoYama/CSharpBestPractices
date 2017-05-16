29e663a34b784337c83a0e813f3eb6dae91e9692
Rick Hysell <rhysell@charter.net>
2017-05-15 23:14:10 -0400

    third commit

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,63 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages product carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            WriteLine("Product instance created");
+        }
+
+        public Product(int productId,
+                        string productName,
+                        string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+
+            WriteLine("Product instance has a name: " + ProductName);
+        }
+
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName +
+                    " (" + ProductId + "): " +
+                    Description;
+        }
+
+    }
+}
